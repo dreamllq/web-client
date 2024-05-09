c099ca563932369edbda1b70e71f8c17dafb3db1
lvliqi <lvliqi_d@163.com>
2024-05-09 10:58:44 +0800

    feat: baidu message

--- src/views/components/ai-management/baidu/session/chat/window.vue
+++ src/views/components/ai-management/baidu/session/chat/window.vue
@@ -1,7 +1,23 @@
 <template>
   <div>
-    <div style='height: 400px;'>
+    <div class='content-block'>
       {{ sessionId }}
+      <template v-for='(item,index) in messageList' :key='index'>
+        <p
+          class='content'
+          :class='{
+            "f-right": item.data.role === "user"
+          }'>
+          <template v-if='item.data.status ==="done"'>
+            <span class='content-inner'>
+              {{ item.data.content }}
+            </span>
+          </template>
+          <template v-else>
+            <span class='content-inner'>...</span>
+          </template>
+        </p>
+      </template>
     </div>
     <div>
       <el-row :gutter='12'>
@@ -22,8 +38,10 @@
 import { AiBaiduMessageService } from '@/services/api';
 import { useMessageSocketClient } from '@/services/ws-client/message-client';
 import { onUnmounted, ref } from 'vue';
+import { useBaiduMessageNotify } from './notify-hook';
 
 const { joinRoom, leaveRoom } = useMessageSocketClient();
+
 const props = defineProps({
   sessionId: {
     type: String,
@@ -37,6 +55,20 @@ onUnmounted(() => {
 });
 
 const content = ref('');
+const messageList = ref<any[]>([]);
+
+useBaiduMessageNotify(props.sessionId, {
+  messageCb: (message) => {
+    messageList.value.push(message);
+  },
+  messageChangeCb: (message) => {
+    messageList.value.forEach((item, index) => {
+      if (item.data.id === message.data.id) {
+        messageList.value[index] = message;
+      }
+    });
+  }
+});
 
 const send = async () => {
   await AiBaiduMessageService.create({
@@ -45,9 +77,32 @@ const send = async () => {
       sessionId: props.sessionId 
     } 
   });
+  content.value = '';
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.content-block{
+  height: 400px;
+  overflow: auto;
+  .content{
+    padding-right: 100px;
+    .content-inner{
+      border: 1px solid #333;
+      background-color: #eee;
+      color: #333;
+      font-size: 12px;
+      line-height: 1.4;
+      padding: 8px;
+      border-radius: 4px;
+      display: inline-block;
+    }
 
+    &.f-right{
+      text-align: right;
+      padding-right: 0;
+      padding-left: 100px; 
+    }
+  }
+}
 </style>
